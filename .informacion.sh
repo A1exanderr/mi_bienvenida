287c3dabcc7eb48df1886e1507434e4aef3c478e
Alex <looveyouuu10@gmail.com>
2025-08-01 10:47:22 -0400

    informacion

--- .informacion.sh
+++ .informacion.sh
@@ -12,5 +12,25 @@ Servidor 2
 ip: 10.1.2.238
 #===============
 
-
-Servidor
+#================================
+Servidor1
+Numero de usuarios es 2500
+en un segundo 
+ilimitado
+#===============================
+Servidor1
+Numero de usuarios es 5000
+en un segundo 
+ilimitado
+#===============================
+#===============================
+#===============================
+#================MONITOR DE RECURSOS DEL SERVIDOR===============
+top            # Uso general del sistema
+htop           # Vista avanzada del uso de CPU/RAM
+vmstat         # Estadísticas de memoria, swap, CPU
+iostat         # Uso de disco
+netstat        # Conexiones de red
+dstat          # Estadísticas combinadas
+#sudo apt install htop
+#===============================================================
