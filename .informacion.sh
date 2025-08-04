0903a869aa1d47705914cc58cf26533c47a50c43
Alex <looveyouuu10@gmail.com>
2025-08-04 10:50:41 -0400

    informacion

--- .informacion.sh
+++ .informacion.sh
@@ -34,3 +34,12 @@ netstat        # Conexiones de red
 dstat          # Estadísticas combinadas
 #sudo apt install htop
 #===============================================================
+
+
+
+
+sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
